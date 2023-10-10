b46f3db196593a6345e35ffc4fe2256f8aecdd57
GuidoPasseggi <guido.passeggi@gmail.com>
2023-10-10 17:56:05 -0300

    05-yt-context-useReducer/ 2v

--- 05-yt-context-useReducer/src/App.jsx
+++ 05-yt-context-useReducer/src/App.jsx
@@ -5,11 +5,15 @@ import Lista from "./components/Lista";
 
 function App() {
   console.log("Link del video: \n https://www.youtube.com/watch?v=j2-X0Pck9jk");
+  console.log(
+    "Desde el minuto 18 dejé de seguir al profesor exacto, \n tratando de replicar lo que el profe de ORT hace"
+  );
 
   return (
     <div className='container mt-3'>
       <Presupuesto></Presupuesto>
       <Formulario></Formulario>
+      <hr />
       <Lista></Lista>
     </div>
   );
